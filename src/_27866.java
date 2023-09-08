a5a6bc5ca5df7afadfe0c6a8ace3b019990bc891
ohjuyoungg <ejrm12@naver.com>
2023-09-08 22:52:53 +0900

    problem-solving

---
+++ src/_27866.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.*;
+
+
+class _27866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        String var = br.readLine();
+
+        String a = line;
+        String[] b = var.split(" ");
+
+        int i = Integer.parseInt(b[0]);
+
+        System.out.println(a.charAt(i-1));
+
+        }
+    }
+
