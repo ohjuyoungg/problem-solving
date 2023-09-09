99a5364112bc6661cc797ed0542a834112f496bc
ohjuyoungg <ejrm12@naver.com>
2023-09-09 23:05:12 +0900

    problem-solving

---
+++ src/_11720.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// 다시 풀기
+
+public class _11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String y = sc.next();
+        sc.close();
+
+        for (int i = 0; i < x; i++) {
+         //   c1++ -'0';
+
+        }
+    }
+}
+
--- src/_2753.java
+++ src/_2753.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class _2735 { // 제출할 때 클래스 이름 반드시 Main으로 바꿔서 제출해야 함. (백준만)
+class _2753 { // 제출할 때 클래스 이름 반드시 Main으로 바꿔서 제출해야 함. (백준만)
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
