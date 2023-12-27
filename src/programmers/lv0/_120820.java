d4e23d97ef3bc9c9ae2a4febda4da55095b26ade
ohjuyoungg <ejrm12@naver.com>
2023-12-27 17:11:53 +0900

    프로그래머스: 나이 출력

---
+++ src/programmers/lv0/_120820.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class _120820 {
+    public static int solution(int age) {
+        int answer = (2022 - age) + 1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+    }
+}
