873160827555e87021c8f757fc761b09ae4fd52a
ohjuyoungg <ejrm12@naver.com>
2023-12-27 17:04:01 +0900

    프로그래머스: 몫 구하기

---
+++ src/programmers/lv0/_120805.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+class _120805 {
+    public static int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(4, 2));
+    }
+}
